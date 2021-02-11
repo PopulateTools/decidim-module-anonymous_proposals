75f68062aa873879661926d1ad273fa5bcfd1dca
Eduardo Martínez <eduardomech@gmail.com>
2021-02-11 22:54:37 +0100

    Remove component-like code for module

--- lib/decidim/anonymous_proposals.rb
+++ lib/decidim/anonymous_proposals.rb
@@ -3,11 +3,9 @@
 require "decidim/anonymous_proposals/admin"
 require "decidim/anonymous_proposals/engine"
 require "decidim/anonymous_proposals/admin_engine"
-require "decidim/anonymous_proposals/component"
 
 module Decidim
-  # This namespace holds the logic of the `AnonymousProposals` component. This component
-  # allows users to create anonymous_proposals in a participatory space.
+  # This namespace holds the logic of the `AnonymousProposals` module
   module AnonymousProposals
   end
 end
