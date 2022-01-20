11f93677b920f061d4d4f465d9e874748df37aff
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2022-01-20 12:07:07 +0100

    Update decidim_version

--- lib/decidim/anonymous_proposals/version.rb
+++ lib/decidim/anonymous_proposals/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-meetings version.
   module AnonymousProposals
     def self.version
-      "0.25.1"
+      "0.25.2"
     end
 
     def self.decidim_version
-      "0.25.1"
+      ">= 0.25.0"
     end
   end
 end
