c3b5b4ae9b7adbc0a32b5d8849f8ab4903751c5f
Thomas Burkhalter <new.mysteryman@gmail.com>
2023-03-13 15:19:48 +0100

    Bump version to 0.27.0

--- lib/decidim/anonymous_proposals/version.rb
+++ lib/decidim/anonymous_proposals/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-meetings version.
   module AnonymousProposals
     def self.version
-      "0.26.1"
+      "0.27.0"
     end
 
     def self.decidim_version
-      ">= 0.26.1"
+      ">= 0.27.0"
     end
   end
 end
