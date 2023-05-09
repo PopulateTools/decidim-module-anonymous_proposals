7eca0170e9b5bf5d798e58e9e3d7c51bade9ae94
Fernando Blat <fernando@blat.es>
2023-05-09 10:33:46 +0200

    Upgrade Decidim version to 0.26.5

--- lib/decidim/anonymous_proposals/version.rb
+++ lib/decidim/anonymous_proposals/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-meetings version.
   module AnonymousProposals
     def self.version
-      "0.25.1"
+      "0.26.5"
     end
 
     def self.decidim_version
-      "0.25.1"
+      "0.26.5"
     end
   end
 end
